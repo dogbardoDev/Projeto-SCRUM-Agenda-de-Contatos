dae580c94eb4ff73e0a916ec675543b06932e765
Anderson da Silva Santos <anderson.ss1090@gmail.com>
2024-10-29 11:47:17 -0300

    mudança de nome

--- src/main/java/org/agprojeto/projetoscrumagendadecontatos/view/TelaContatos.java
+++ src/main/java/org/agprojeto/projetoscrumagendadecontatos/view/TelaContatos.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TelaContatos extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TelaContatos.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TelaContatos.class.getResource("TelaContatos.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 640);
         stage.setResizable(false);
         stage.setTitle("Agenda de Contatos");

ba58ac94e75b54efb6eb5bcf8a3c7313fa35b97d
Gabriel Laet <gabriellaetfm12@gmail.com>
2024-10-31 10:45:18 -0300

    Conexão de elementos em TelaNovoContatoController.java

--- src/main/java/org/agprojeto/projetoscrumagendadecontatos/view/controller/TelaNovoContatoController.java
+++ src/main/java/org/agprojeto/projetoscrumagendadecontatos/view/controller/TelaNovoContatoController.java
@@ -1,4 +1,35 @@
 package org.agprojeto.projetoscrumagendadecontatos.view.controller;
 
-public class TelaNovoContatoController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TelaNovoContatoController implements Initializable {
+
+    @FXML
+    private TextField txtNomeContato;
+    @FXML
+    private TextField txtSobrenomeContato;
+    @FXML
+    private TextField txtNumero1Contato;
+    @FXML
+    private TextField txtNumero2Contato;
+    @FXML
+    private TextField txtEmailContato;
+    @FXML
+    private TextArea txtDescricaoContato;
+    @FXML
+    private Button btnSalvarContato;
+    @FXML
+    private Button btnVoltarContato;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }

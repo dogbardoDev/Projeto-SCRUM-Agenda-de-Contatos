fbf7ad5c102087ad9e3ab6d11a4b3037b242eb04
Gabriel Laet <gabriellaetfm12@gmail.com>
2024-10-31 12:51:43 -0300

    Salvar contato inicialmente implementado

--- src/main/java/org/agprojeto/projetoscrumagendadecontatos/view/App.java
+++ src/main/java/org/agprojeto/projetoscrumagendadecontatos/view/App.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class app extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(app.class.getResource("TelaNovoContato.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("TelaNovoContato.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 640);
         stage.setResizable(false);
         stage.setTitle("Agenda de Contatos");
--- src/main/java/org/agprojeto/projetoscrumagendadecontatos/view/controller/TelaNovoContatoController.java
+++ src/main/java/org/agprojeto/projetoscrumagendadecontatos/view/controller/TelaNovoContatoController.java
@@ -5,6 +5,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.agprojeto.projetoscrumagendadecontatos.controller.ContatoController;
+import org.agprojeto.projetoscrumagendadecontatos.dao.ContatoDAO;
+import org.agprojeto.projetoscrumagendadecontatos.dao.DAOFactory;
+import org.agprojeto.projetoscrumagendadecontatos.dto.ContatoDTO;
+import org.agprojeto.projetoscrumagendadecontatos.mapper.ContatoMapper;
+import org.agprojeto.projetoscrumagendadecontatos.model.entities.Contato;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,6 +33,30 @@ public class TelaNovoContatoController implements Initializable {
     private Button btnSalvarContato;
     @FXML
     private Button btnVoltarContato;
+
+    private final ContatoController contatoController = new ContatoController();
+
+    @FXML
+    private void salvarContato() {
+        String nomeContato = txtNomeContato.getText();
+        String sobrenomeContato = txtSobrenomeContato.getText();
+        String numero1Contato = txtNumero1Contato.getText();
+        String numero2Contato = txtNumero2Contato.getText();
+        String emailContato = txtEmailContato.getText();
+        String descricaoContato = txtDescricaoContato.getText();
+
+        ContatoDTO contato = new ContatoDTO(
+                null,
+                nomeContato,
+                sobrenomeContato,
+                numero1Contato,
+                numero2Contato,
+                emailContato,
+                descricaoContato
+        );
+
+        contatoController.inserirContato(contato);
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
